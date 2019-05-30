d9df581dbd4718c16559620b21f114e26577b391
Austin Burgard <awburgard@gmail.com>
2019-05-30 11:20:32 -0500

    renders secrets based on secrecy level

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -3,8 +3,8 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    const queryString = `SELECT * FROM "secret" WHERE "secrecy_level" <= $1;`;
+    pool.query(queryString, [req.user.clearance_level])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
