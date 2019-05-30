4bc49316b13246cd561096af3b34b856c7bf5eb2
Austin Burgard <awburgard@gmail.com>
2019-05-30 11:28:30 -0500

    restricts access to secrets page

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
     const queryString = `SELECT * FROM "secret" WHERE "secrecy_level" <= $1;`;
-    pool.query(queryString, [req.user.clearance_level])
-        .then(results => res.send(results.rows))
-        .catch(error => {
-            console.log('Error making SELECT for secrets:', error);
-            res.sendStatus(500);
-        });
+    if (req.user.isAuthenticated()) {
+        pool.query(queryString, [req.user.clearance_level])
+            .then(results => res.send(results.rows))
+            .catch(error => {
+                console.log('Error making SELECT for secrets:', error);
+                res.sendStatus(500);
+            });
+    } else {
+        sendStatus(403);
+    }
 });
 
 module.exports = router;
